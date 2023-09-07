855f37d360b0b7ab4ab30954724267ce9627c790
Tejusajayan <113707804+Tejusajayan@users.noreply.github.com>
2023-09-07 19:08:47 +0530

    Delete ASSETS directory

--- ASSETS/JS/singleproduct.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $('.add-btn').click(function(e) {
-        e.preventDefault();
-        var itemId = $(this).data('item-id');
-        window.location.href='/product/'+itemId+'/'
-    })      
-});
